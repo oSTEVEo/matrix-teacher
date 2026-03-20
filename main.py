4c6d2d0f1e21936226f386212185536db387ef42
oSTEVEo <steve.games@bk.ru>
2026-03-20 17:55:23 +0300

    Added code

---
+++ main.py
@@ -0,0 +1,67 @@
+from typing import Union
+
+from fastapi import FastAPI, HTTPException
+import uvicorn
+import numpy as np
+from pydantic import BaseModel
+
+from tasks import *
+
+app = FastAPI()
+task_storage = TaskRepository()
+
+class DeterminantAnswerSchema(BaseModel):
+    value: int
+
+class SLAUAnswerSchema(BaseModel):
+    value: list
+
+AnswerUnion = Union[DeterminantAnswerSchema, SLAUAnswerSchema]
+
+@app.get("/ping")
+def ping():
+    return {"ok" : True}
+
+
+# Tasks
+@app.post("/tasks/determinant", tags=["Users"], summary="Получить задание по решению определителя")
+def send_determinant(matrix_width:int=3, matrix_height:int=3):
+    matrix = np.random.randint(-10, 10, (matrix_width, matrix_height))
+    task = DeterminantTask(matrix)
+    task_storage.save(task)
+    return {"task_id": task.id, "question": task.get_question()}
+
+@app.post("/tasks/slau", tags=["Users"], summary="Получить задание по решению СЛАУ")
+def send_slau(variables:int=3):
+    coefficients = np.random.randint(-10, 10, (variables, variables))
+    biases = np.random.randint(-10, 10, (variables, 1))
+    task = SLAUTask(coefficients, biases)
+    task_storage.save(task)
+    return {"task_uuid": task.id, "question": task.get_question()}
+
+
+# Answers
+@app.post("/answers/{task_uuid}", summary="Проверить правильность ответа")
+def check_answer(task_uuid: str, answer: AnswerUnion):
+    task = task_storage.find_by_id(task_uuid)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task with the given uuid was not found.")
+    solution = task.get_answer()
+    if type(solution) != type(answer.value):
+        raise HTTPException(status_code=415, detail="Incorrect data type in answer field.")
+
+    if solution == answer.value:
+        return {"ok" : True}
+    return {"ok" : False}
+
+@app.get("/answers/{task_uuid}", summary="Получить верный ответ", tags=["Admin"])
+def get_true_answer(task_uuid: str):
+    task = task_storage.find_by_id(task_uuid)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task with the given uuid was not found.")
+    solution = task.get_answer()
+    return {"answer" : solution}
+    
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
---
+++ tasks.py
@@ -0,0 +1,65 @@
+import uuid
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+import numpy as np
+
+class Task(ABC):
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+    @abstractmethod
+    def get_question():
+        """Возвращает условие задачи для отображения пользователю."""
+        pass
+
+    @abstractmethod
+    def get_answer(self):
+        """Возвращает правильный ответ (число, вектор, матрицу и т.д.)."""
+        pass
+
+class DeterminantTask(Task):
+    def __init__(self, matrix: np.ndarray):
+        super().__init__()
+        self.matrix = matrix
+        # Здесь можно сразу вычислить ответ
+        self._solution = self._compute_determinant()
+
+    def _compute_determinant(self):
+        matrix_np = self.matrix
+        return np.linalg.det(matrix_np)
+
+    def get_question(self):
+        return self.matrix.tolist()
+
+    def get_answer(self):
+        return self._solution
+
+class SLAUTask(Task):
+    def __init__(self, coef_matrix: np.ndarray, depvar_matrix: np.ndarray):
+        super().__init__()
+        self.coef_matrix = coef_matrix
+        self.depvar_matrix = depvar_matrix
+        self._system_solutions = self._solve_system()
+
+    def _solve_system(self):
+        return np.linalg.solve(self.coef_matrix, self.depvar_matrix)
+
+    def get_question(self):
+        return {"coefficients": self.coef_matrix.tolist(), "biases": self.depvar_matrix.tolist()}
+
+    def get_answer(self):
+        return self._system_solutions.flatten().tolist()
+
+class TaskRepository:
+    def __init__(self):
+        self._storage: Dict[str, Task] = {}  # in-memory хранилище
+
+    def save(self, task: Task):
+        self._storage[task.id] = task
+
+    def find_by_id(self, task_id: str) -> Optional[Task]:
+        return self._storage.get(task_id)
+
+    def remove(self, task_id: str):
+        if task_id in self._storage:
+            del self._storage[task_id]
